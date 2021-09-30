09c69039ca21d5615c7ebcd788b8254d33fc5eec
melicaso1624 <melicarmona35@gmail.com>
2021-09-29 23:50:34 -0500

    :pencil:

--- server.js
+++ server.js
@@ -11,19 +11,28 @@ const PORT = 8080;
 const container = require('./Container');
 const container_class = new container('./productos.txt');
 
+/** Welcome greeting */
 app.get("/", (request, response, next) => {
     response.send("<h1>Bienvenido!!!</h1>");
 });
 
-
+/** List products in an array */
 app.get("/productos", (request, response, next) => {
+
     let container_file = container_class.read();
+
     response.send(container_file);
 });
+
+/** Show product randowly */
 app.get("/productoRandom", (request, response, next) => {
+
     let container_file = container_class.read();
+
     let productRandom = container_file[Math.floor(Math.random() * container_file.length)];
+
     response.send(productRandom);
 });
+
 const server = app.listen(PORT, () => { console.log(`Servidor http escuchando en el puerto ${PORT}`); });
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
